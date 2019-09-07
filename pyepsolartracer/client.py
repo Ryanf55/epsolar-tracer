6d0999c976cad822d877de78f97e2f30140620ac
RyanF55 <ryanfriedman5410@gmail.com>
2019-09-07 16:45:51 -0600

    Support setting load output easier

--- pyepsolartracer/client.py
+++ pyepsolartracer/client.py
@@ -10,6 +10,8 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.mei_message import *
 from pyepsolartracer.registers import registerByName
 
+import time
+
 
 import logging
 logging.basicConfig()
@@ -32,7 +34,7 @@ class EPsolarTracerClient:
         '''
         self.unit = unit
         if serialclient is None:
-            port = kwargs.get('port', '/dev/ttyXRUSB0')
+            port = kwargs.get('port', 'COM1')
             baudrate = kwargs.get('baudrate', 115200)
             self.client = ModbusClient(
                 method="rtu",
@@ -82,10 +84,10 @@ class EPsolarTracerClient:
             self.client.write_coil(register.address, values, unit=self.unit)
             response = True
         elif register.is_discrete_input():
-            _logger.error("Cannot write discrete input " + repr(name))
+            log.error("Cannot write discrete input " + repr(name))
             pass
         elif register.is_input_register():
-            _logger.error("Cannot write input register " + repr(name))
+            log.error("Cannot write input register " + repr(name))
             pass
         else:
             self.client.write_registers(register.address, values, unit=self.unit)
@@ -96,3 +98,89 @@ class EPsolarTracerClient:
 __all__ = [
     "EPsolarTracerClient",
 ]
+
+
+class EPsolarTracerClientExtended(EPsolarTracerClient):
+    def __init__(self, unit=1, serialclient=None, **kwargs):
+        super().__init__(unit=1, serialclient=None, **kwargs)
+
+        #Set this to 1 to allow user to use "Enter" button to control load at the same time as the script running
+        self.allow_manual_load_control = kwargs.get('allow_manual_load_control', 0)
+        self.default_load_state = kwargs.get('default_load_state',None)
+        print("Default was ...",self.read_input("Default Load On/Off in manual mode").value)
+        
+
+
+        self.mqtt_broker_ip = kwargs.get('mqtt_broker_ip','DISABLED')
+
+        if self.default_load_state is None:
+            if self.read_input("Default Load On/Off in manual mode").value == 1:
+                self.default_load_state = 1
+                print("Default Load is on. Setting test mode state to on.")
+            else:
+                self.default_load_state = 0
+                print("Default Load is off. Setting test mode state to off.")
+        else:
+            self.set_default_load_state(self.default_load_state)
+        
+        # Enable load test mode if allow_manual_load_control is off 
+        # Because there is no way to query the current load state, keep track of it in the code. 
+        if self.allow_manual_load_control == 0:
+            self._write_load_test_mode_enabled(1)
+            print('Test mode enabled from now on')
+             
+
+    
+    def write_load_state(self,load_state):
+        if self.allow_manual_load_control == 1:
+
+            # Turn on test mode, set the state, turn off test mode
+            self._write_load_test_mode_enabled(1)
+            if load_state == 1:
+                print("ON")
+                self.write_output("Force the load on/off",1)
+            elif load_state == 0:
+                print("OFF")
+                self.write_output("Force the load on/off",0)
+            else:
+                print("Error. Unsupported Load State")
+            self._write_load_test_mode_enabled(1)
+
+        else:
+            #Already in test mode. Just set the state
+            if load_state == 1:
+                print("ON2")
+                self.write_output("Force the load on/off",1)
+            elif load_state == 0:
+                print("OFF2")
+                self.write_output("Force the load on/off",0)
+            else:
+                print("Error. Unsupported Load State")
+
+
+    def set_load_to_default_load_state(self):
+        self.write_load_state(self.default_load_state)
+
+    def _write_load_test_mode_enabled(self,load_test_mode_enabled):
+        if load_test_mode_enabled == 1:
+            self.write_output("Enable load test mode",1)
+        elif load_test_mode_enabled == 0:
+            self.write_output("Enable load test mode",0)
+        else:
+            print("Error. Unsupported load test mode")
+
+    
+    def set_default_load_state(self,default_load_state):
+        if default_load_state == 1:
+            self.write_output("Default Load On/Off in manual mode",1)
+        elif default_load_state == 0:
+            self.write_output("Default Load On/Off in manual mode",0)
+        else:
+            print("Error. Unsupported load test mode")
+
+    def close(self):
+        ''' Closes the underlying connection
+        '''
+        self.set_load_to_default_load_state()
+        return self.client.close()
+
